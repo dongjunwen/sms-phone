56304fa59c66405160fe1b3f06590626fc931b4b
donglu <donglu@cs.sh.cn>
2018-08-03 18:47:36 +0800

    调整

--- sms-phone/src/main/java/com/bootdo/BootdoApplication.java
+++ sms-phone/src/main/java/com/bootdo/BootdoApplication.java
@@ -23,14 +23,6 @@ public class BootdoApplication {
     public static void main(String[] args) {
         SpringApplication springApplication=new SpringApplication(BootdoApplication.class);
         springApplication.run(args);
-        System.out.println("ヾ(◍°∇°◍)ﾉﾞ    3911启动成功      ヾ(◍°∇°◍)ﾉﾞ\n" +
-                " ______                    _   ______            \n" +
-                "|_   _ \\                  / |_|_   _ `.          \n" +
-                "  | |_) |   .--.    .--. `| |-' | | `. \\  .--.   \n" +
-                "  |  __'. / .'`\\ \\/ .'`\\ \\| |   | |  | |/ .'`\\ \\ \n" +
-                " _| |__) || \\__. || \\__. || |, _| |_.' /| \\__. | \n" +
-                "|_______/  '.__.'  '.__.' \\__/|______.'  '.__.'  ");
-
     }
 
 
